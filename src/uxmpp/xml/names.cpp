9e20ed8cfab72132adcc1bd2cda8b78885e0322a
Dan Arrhenius <dan@zaark.com>
2014-12-21 15:19:39 +0100

    Added missing files

---
+++ src/uxmpp/xml/names.cpp
@@ -0,0 +1,102 @@
+/*
+ *  Copyright (C) 2014 Ultramarin Design AB <dan@ultramarin.se>
+ *
+ *  This file is part of uxmpp.
+ *
+ *  uxmpp is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <uxmpp/xml/names.hpp>
+
+
+UXMPP_START_NAMESPACE2(uxmpp, xml)
+
+//
+// Uxmpp internal
+//
+const std::string namespace_uxmpp_error {"http://ultramarin.se/uxmpp#internal-error"};
+const std::string namespace_uxmpp_timer {"http://ultramarin.se/uxmpp#internal-timer"};
+
+//
+// stream
+//
+const std::string namespace_stream {"http://etherx.jabber.org/streams"};
+const std::string alias_stream     {"stream"};
+const std::string tag_stream       {"stream"};
+const std::string full_tag_stream  {"http://etherx.jabber.org/streams:stream"};
+
+//
+// features
+//
+const std::string tag_features      {"features"};
+const std::string full_tag_features {"http://etherx.jabber.org/streams:features"};
+
+//
+// jabber:client
+//
+const std::string namespace_jabber_client {"jabber:client"};
+
+//
+// bind
+//
+const std::string namespace_bind {"urn:ietf:params:xml:ns:xmpp-bind"};
+const std::string tag_bind       {"bind"};
+
+//
+// namespace xmpp-streams
+//
+const std::string namespace_xmpp_streams {"urn:ietf:params:xml:ns:xmpp-streams"};
+
+//
+// error
+//
+const std::string full_tag_error {"http://etherx.jabber.org/streams:error"};
+
+//
+// IQ stanza
+//
+const std::string full_tag_iq_stanza {"jabber:client:iq"};
+
+//
+// Error stanza
+//
+const std::string full_tag_error_stanza {"jabber:client:error"};
+
+//
+// Presence stanza
+//
+const std::string full_tag_presence_stanza {"jabber:client:presence"};
+
+//
+// Message stanza
+//
+const std::string full_tag_message_stanza {"jabber:client:message"};
+
+//
+// IQ roster namespace
+//
+const std::string namespace_iq_roster {"jabber:iq:roster"};
+
+//
+// Roster item full tag
+//
+const std::string full_tag_roster_item {"jabber:iq:roster:item"};
+
+//
+// Roster group full tag
+//
+const std::string full_tag_roster_group {"jabber:iq:roster:group"};
+
+
+
+UXMPP_END_NAMESPACE2
---
+++ src/uxmpp/xml/names.hpp
@@ -0,0 +1,156 @@
+/*
+ *  Copyright (C) 2014 Ultramarin Design AB <dan@ultramarin.se>
+ *
+ *  This file is part of uxmpp.
+ *
+ *  uxmpp is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef UXMPP_XML_NAMES_HPP
+#define UXMPP_XML_NAMES_HPP
+
+#include <uxmpp/types.hpp>
+#include <string>
+
+
+namespace uxmpp { namespace xml {
+
+        /**
+         * XML namespace for uxmpp internal errors:
+         * <code>http://ultramarin.se/uxmpp#internal-error</code>.
+         */
+        extern const std::string namespace_uxmpp_error;
+
+        /**
+         * XML namespace for uxmpp internal timers.
+         * <code>http://ultramarin.se/uxmpp#internal-timer</code>.
+         */
+        extern const std::string namespace_uxmpp_timer;
+/*
+const std::string XmlUxmppInternalErrorNs = "http://ultramarin.se/uxmpp#internal-error";
+const std::string XmlUxmppInternalErrorDefNs = XmlUxmppInternalErrorNs;
+
+// uxmpp internal timer namespace
+//
+const std::string XmlUxmppInternalTimerNs = "http://ultramarin.se/uxmpp#internal-timer";
+const std::string XmlUxmppInternalTimerDefNs = XmlUxmppInternalTimerNs;
+const std::string XmlUxmppInternalTimerTagFull = XmlUxmppInternalTimerNs + std::string(":timeout");
+*/
+        /**
+         * XML namespace for the 'stream' tag: <code>http://etherx.jabber.org/streams</code>.
+         */
+        extern const std::string namespace_stream;
+        /**
+         * XML alias for the stream namespace: <code>stream</code>.
+         */
+        extern const std::string alias_stream;
+        /**
+         * XML tag name for the stream tag: <code>stream</code>.
+         */
+        extern const std::string tag_stream;
+        /**
+         * Fully qualified XML tag name for the stream tag:
+         * <code>http://etherx.jabber.org/streams:stream</code>.
+         */
+        extern const std::string full_tag_stream;
+
+
+        /**
+         * XML tag name for the features tag: <code>features</code>.
+         */
+        extern const std::string tag_features;
+        /**
+         * Fully qualified XML tag name for the features tag:
+         * <code>http://etherx.jabber.org/streams:features</code>.
+         */
+        extern const std::string full_tag_features;
+
+
+        /**
+         * XML jabber client namespace: <code>jabber:client</code>.
+         */
+        extern const std::string namespace_jabber_client;
+
+
+        /**
+         * XML bind namespace: <code>urn:ietf:params:xml:ns:xmpp-bind</code>.
+         */
+        extern const std::string namespace_bind;
+        /**
+         * XML bind tag: <code>bind</code>.
+         */
+        extern const std::string tag_bind;
+
+
+        /**
+         * XMPP-streams namespace: <code>urn:ietf:params:xml:ns:xmpp-streams</code>.
+         */
+        extern const std::string namespace_xmpp_streams;
+
+
+        /**
+         * Fully qualified XML tag name for the error tag:
+         * <code>http://etherx.jabber.org/streams:error</code>.
+         */
+        extern const std::string full_tag_error;
+
+
+        /**
+         * Fully qualified XML tag name for the IQ stanza tag:
+         * <code>jabber:client:iq</code>.
+         */
+        extern const std::string full_tag_iq_stanza;
+
+
+        /**
+         * Fully qualified XML tag name for the error stanza tag:
+         * <code>jabber:client:error</code>.
+         */
+        extern const std::string full_tag_error_stanza;
+
+
+        /**
+         * Fully qualified XML tag name for the presence stanza tag:
+         * <code>jabber:client:presence</code>.
+         */
+        extern const std::string full_tag_presence_stanza;
+
+
+        /**
+         * Fully qualified XML tag name for the message stanza tag:
+         * <code>jabber:client:message</code>.
+         */
+        extern const std::string full_tag_message_stanza;
+
+
+        /**
+         * IQ roster namespace: <code>jabber:iq:roster</code>.
+         */
+        extern const std::string namespace_iq_roster;
+
+        /**
+         * Fully qualified XML tag name for the roster item tag:
+         * <code>jabber:iq:roster:item</code>.
+         */
+        extern const std::string full_tag_roster_item;
+
+        /**
+         * Fully qualified XML tag name for the roster group tag:
+         * <code>jabber:iq:roster:group</code>.
+         */
+        extern const std::string full_tag_roster_group;
+
+}}
+
+
+#endif
